2b432896ce6d619d453cfaf20a0a44fa7a1c1127
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-04-02 16:59:59 +0900

    d

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -9,12 +9,12 @@ use Abraham\TwitterOAuth\TwitterOauth;
 
 class TwitterController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
         //ツイートを5件取得
         $result = \Twitter::get('lists/statuses',['list_id' => '1377874159393337347'], array("count" => 200));
 
-        //ViewのTwitter.blade.phpに渡す
+        //api twitter
         return
         response()->json([
             'message' => 'OK',
