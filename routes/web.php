08b9d20d800fefe49cd3cb78f0b7b1fa29ecd6a8
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-04-02 16:55:04 +0900

    d

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('twitter', 'App\Http\Controllers\TwitterController@index');
+/*Route::get('twitter', 'App\Http\Controllers\TwitterController@index');*/
